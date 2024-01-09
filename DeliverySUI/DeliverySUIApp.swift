c6a736b855bb40d3b7335387979a8d875fac4382
d.o.kononov <d.o.kononov@gmail.com>
2024-01-09 10:17:59 +0300

    Initial Commit

---
+++ DeliverySUI/DeliverySUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeliverySUIApp.swift
+//  DeliverySUI
+//
+//  Created by Dmitry Kononov on 9.01.24.
+//
+
+import SwiftUI
+
+@main
+struct DeliverySUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
